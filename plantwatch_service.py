68c4d4950f56a537de52c29ffbfe14198f5352dc
Harry Kelly <hrykly@gmail.com>
2023-03-07 12:40:46 +0000

    Refactored service code

--- plantwatch_service.py
+++ plantwatch_service.py
@@ -10,23 +10,25 @@ logging.basicConfig(level=logging.INFO)
 
 env_vars = dotenv_values(".env")
 
-def readingReceived(ch, method, properties, body):
-    logging.info(f"Received a reading: {body}")
-    reading = ast.literal_eval(body.decode('UTF-8'))
-    readings.insert_one(reading)
+class PlantwatchService:
+    def __init__(self):
+        self.client = MongoClient(env_vars["MONGO_SERVER"], 27017)
+        self.db = self.client.plantwatch
+        self.readings = self.db.readings
+        self.creds = pika.credentials.PlainCredentials(env_vars["RABBITMQ_USER"], env_vars["RABBITMQ_PASSWORD"], erase_on_connect=False)
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(env_vars["RABBITMQ_SERVER"], credentials=self.creds))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare("plantwatch_readings")
+        self.channel.basic_consume(queue="plantwatch_readings", auto_ack=True, on_message_callback=self.readingReceived)
+        self.channel.start_consuming()
 
-client = MongoClient(env_vars["MONGO_SERVER"], 27017)
-db = client.plantwatch
-readings = db.readings
-
-creds = pika.credentials.PlainCredentials(env_vars["RABBITMQ_USER"], env_vars["RABBITMQ_PASSWORD"], erase_on_connect=False)
-connection = pika.BlockingConnection(pika.ConnectionParameters(env_vars["RABBITMQ_SERVER"], credentials=creds))
-channel = connection.channel()
+    def readingReceived(self, ch, method, properties, body):
+        logging.info(f"Received a reading: {body}")
+        reading = ast.literal_eval(body.decode('UTF-8'))
+        self.readings.insert_one(reading)
 
 def main():
-    channel.queue_declare("plantwatch_readings")
-    channel.basic_consume(queue="plantwatch_readings", auto_ack=True, on_message_callback=readingReceived)
-    channel.start_consuming()
+    plantwatch = PlantwatchService()
 
 if (__name__ == "__main__"):
     main()
