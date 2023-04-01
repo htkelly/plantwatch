5930a6fe4d58e3fbeba073cb919ba15dca007903
htkelly <hrykly@gmail.com>
2023-04-01 15:42:59 +0000

    More API auth improvements and bugfixes

--- plantwatch_api/bin/plantwatch_api.dart
+++ plantwatch_api/bin/plantwatch_api.dart
@@ -36,7 +36,7 @@ main(List<String> arguments) async {
     } else if (request.uri.path == "/devices" && request.method == 'GET') {
       try {
         var auth = request.headers['Authorization'];
-        if (auth == null){
+        if (auth == null) {
           request.response.statusCode = HttpStatus.unauthorized;
           request.response.write('Unauthorized');
           request.response.close();
@@ -57,7 +57,6 @@ main(List<String> arguments) async {
           request.response.statusCode = HttpStatus.OK;
           request.response.write(json.encode(devices));
           request.response.close();
-          return;
         }
       } catch (e) {
         request.response.statusCode = HttpStatus.internalServerError;
@@ -65,8 +64,9 @@ main(List<String> arguments) async {
         request.response.close();
       }
       // This handler is required so that this route will work without disabling CORS/web security on the frontend application
-    } else if (request.uri.path.startsWith("/devices") &&
-        request.method == 'OPTIONS') {
+    } else if (request.uri.path.startsWith("/devices") ||
+        request.uri.path.startsWith("/readings") &&
+            request.method == 'OPTIONS') {
       request.response.statusCode = HttpStatus.OK;
       request.response.close();
       // This handler sets parameters on a device
@@ -76,7 +76,7 @@ main(List<String> arguments) async {
       try {
         var deviceId = request.uri.pathSegments[1];
         var auth = request.headers['Authorization'];
-          if (auth == null){
+        if (auth == null) {
           request.response.statusCode = HttpStatus.unauthorized;
           request.response.write('Unauthorized');
           request.response.close();
@@ -118,7 +118,7 @@ main(List<String> arguments) async {
       try {
         var deviceId = request.uri.pathSegments[1];
         var auth = request.headers['Authorization'];
-        if (auth == null){
+        if (auth == null) {
           request.response.statusCode = HttpStatus.unauthorized;
           request.response.write('Unauthorized');
           request.response.close();
@@ -153,7 +153,7 @@ main(List<String> arguments) async {
         var deviceId = request.uri.pathSegments[1];
         var device = await deviceCollection.findOne(where.eq("_id", deviceId));
         var auth = request.headers['Authorization'];
-        if (auth == null){
+        if (auth == null) {
           request.response.statusCode = HttpStatus.unauthorized;
           request.response.write('Unauthorized');
           request.response.close();
@@ -174,6 +174,7 @@ main(List<String> arguments) async {
           request.response.close();
           return;
         }
+        request.response.statusCode = HttpStatus.OK;
         request.response.write(await json.encode(await readingCollection
             .find(where.eq("deviceId", deviceId))
             .toList()));
@@ -183,19 +184,50 @@ main(List<String> arguments) async {
         request.response.write('Internal server error');
         request.response.close();
       }
+      // This handler gets a reading by id
     } else if (request.uri.path.startsWith("/readings") &&
-        request.method == 'GET') {
-      if (request.uri.pathSegments.length == 1) {
+        request.method == 'GET' &&
+        request.uri.pathSegments.length == 2) {
+      try {
+        var reading = await readingCollection
+            .findOne(where.eq("_id", request.uri.pathSegments[1]));
+        if (reading == null) {
+          request.response.statusCode = HttpStatus.notFound;
+          request.response.write('Reading not found');
+          request.response.close();
+          return;
+        }
+        var device = await deviceCollection
+            .findOne(where.eq("_id", reading['deviceId']));
+        if (device == null) {
+          request.response.statusCode = HttpStatus.notFound;
+          request.response.write('Device not found');
+          request.response.close();
+          return;
+        }
+        var auth = request.headers['Authorization'];
+        if (auth == null) {
+          request.response.statusCode = HttpStatus.unauthorized;
+          request.response.write('Unauthorized');
+          request.response.close();
+          return;
+        }
+        var token = await fb.auth().verifyIdToken(auth![0].split(' ')[1]);
+        if (token == null ||
+            token.claims == null ||
+            device['userId'] != token.claims.subject) {
+          request.response.statusCode = HttpStatus.unauthorized;
+          request.response.write('Unauthorized');
+          request.response.close();
+          return;
+        }
         request.response.statusCode = HttpStatus.OK;
-        request.response
-            .write(json.encode(await readingCollection.find().toList()));
+        request.response.write(await json.encode(reading));
         request.response.close();
-        return;
-      } else if (request.uri.pathSegments.length == 2) {
-        request.response.write(await json.encode(await readingCollection
-            .findOne(where.eq("_id", request.uri.pathSegments[1]))));
+      } catch (e) {
+        request.response.statusCode = HttpStatus.internalServerError;
+        request.response.write('Internal server error');
         request.response.close();
-        return;
       }
     } else {
       request.response.statusCode = HttpStatus.notFound;
